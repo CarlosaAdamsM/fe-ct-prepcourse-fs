9fe9c962e7cd6c43031dfc4bd27374087cf55aa4
carlosaadamsm <caam2298@gmail.com>
2023-09-04 18:02:44 -0300

    modulo5 parcial

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -96,6 +96,7 @@ function numeroMasGrande(arrayOfNums) {
    // El parámetro "arrayOfNums" es un arreglo de números.
    // Retornar el número más grande.
    // Tu código:
+   
 }
 
 function multiplicarArgumentos() {

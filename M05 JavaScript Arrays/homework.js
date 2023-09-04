cedaf93501f4a6ea4c19d40f081c3c61016975fc
carlosaadamsm <caam2298@gmail.com>
2023-09-04 13:21:25 -0300

    cambios realizados

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -84,12 +84,6 @@ function promedioResultadosTest(resultadosTest) {
    // El parámetro "resultadosTest" es un arreglo de números.
    // Itera (en un bucle) los elementos del arreglo y devuelve el promedio de las notas.
    // Tu código:
-   let resultadoSuma = 0;
-
-   for (i = 0; i < resultadosTest.length; i++) {
-	   resultadosTest += resultadosTest[i]
-	}
-	return resultadoSuma / resultadosTest.length;
 }
 
 function numeroMasGrande(arrayOfNums) {

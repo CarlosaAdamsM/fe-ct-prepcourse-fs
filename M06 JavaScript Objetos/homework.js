12242dd1f8399e3fe46ed6a976c7adb3d76530f6
carlosaadamsm <caam2298@gmail.com>
2023-09-28 18:03:52 -0300

    actualizar

--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -29,6 +29,8 @@ function agregarPropiedad(objeto, propiedad) {
    // Esta propiedad será igual al valor `null`.
    // Retornar el objeto.
    // Tu código:
+   objeto[propiedad] = null;
+   return objeto;
 }
 
 function invocarMetodo(objeto, metodo) {
